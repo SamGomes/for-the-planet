c2979046bdb265662b798a05eddf36c02321a82e
José Bernardo Rocha <jose.bernardo.rocha@gmail.com>
2019-05-23 17:02:06 +0100

    Perform a simple utterance on robotic player

--- Assets/Scripts/Player/AIPlayer/RoboticPlayer.cs
+++ Assets/Scripts/Player/AIPlayer/RoboticPlayer.cs
@@ -6,10 +6,35 @@ using UnityEngine.UI;
 
 public class RoboticPlayer : AIPlayer
 {
+
+    private ThalamusConnector thalamusConnector = null;
+
     public RoboticPlayer(GameObject playerUIPrefab, GameObject playerCanvas, MonoBehaviourFunctionalities playerMonoBehaviourFunctionalities, PopupScreenFunctionalities warningScreenRef, Sprite UIAvatar, int id, string name) :
         base(playerUIPrefab, playerCanvas, playerMonoBehaviourFunctionalities, warningScreenRef, UIAvatar, id, name)
     {
         this.playerSelfDisablerUI.SetActive(true);
+        this.InitThalamusConnectorOnPort(7000, name);
+    }
+
+    public void InitThalamusConnectorOnPort(int port, string name)
+    {
+        thalamusConnector = new ThalamusConnector(port);
+        this.name = name;
+    }
+
+    public void PerformUtterance(string text, string[] tags, string[] values)
+    {
+        thalamusConnector.PerformUtterance(text, tags, values);
+    }
+
+    public void FlushRobotUtterance(string text)
+    {
+       PerformUtterance(text, new string[] { }, new string[] { });
+    }
+
+    public void GazeAt(string target)
+    {
+        thalamusConnector.GazeAt(target);
     }
 
     // Investment
@@ -26,6 +51,8 @@ public class RoboticPlayer : AIPlayer
         yield return InvestInEconomy(economyInvestment);
 
         yield return InvestInEnvironment(environmentInvestment);
+
+        FlushRobotUtterance("OlÁ!!!!");
 
         yield return EndBudgetAllocationPhase();
     }
