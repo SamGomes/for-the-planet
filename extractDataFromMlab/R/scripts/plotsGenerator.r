ea17b56cddce90343858058b1efcb21459ef0d0f
SamGomes <sam.s.gomes@sapo.pt>
2019-12-12 17:52:15 +0000

    Just missing one chart generator

--- extractDataFromMlab/R/scripts/plotsGenerator.r
+++ extractDataFromMlab/R/scripts/plotsGenerator.r
@@ -11,9 +11,6 @@ suppressMessages(library(sjPlot))
 suppressMessages(library(sjmisc))
 
 gameresultslog <- read.csv(file="input/gameresultslog.csv", header=TRUE, sep=",")
-# gameresultslogOutcomes<-split(gameresultslog, gameresultslog$outcome)
-# winOutcome<-gameresultslogOutcomes$VICTORY
-# lossOutcome<-gameresultslogOutcomes$LOSS
 
 plot <- ggplot(gameresultslog, aes(x = gameresultslog$type, fill = gameresultslog$outcome)) + geom_bar(color="black", position="fill") 
 plot <- plot + labs(x = "Player Type", y = "Frequencies (%)", fill = "Game outcome") + theme(axis.text=element_text(size = 15), axis.title=element_text(size = 15, face = "bold")) + scale_x_discrete(labels = as.character(c("Constructive\nCollectivist","Constructive\nIndividualist","Disruptive\nCollectivist","Disruptive\nIndividualistic","Random")))  
@@ -22,11 +19,20 @@ suppressMessages(ggsave(sprintf("plots/OutcomeFrequencies.png")))
 gameresultslogOutcomes<-split(gameresultslog, gameresultslog$outcome)
 winOutcome<-gameresultslogOutcomes$VICTORY
 agg <- aggregate(pos ~ type, winOutcome, mean)
-print(agg)
-plot <- ggplot(agg, aes(x = agg$type, y=agg$pos)) + geom_bar(color="black", stat="identity") 
+plot <- ggplot(agg, aes(x = agg$type, y=agg$pos)) + geom_bar(color="black", stat="identity", fill = "#638ad3") 
 plot <- plot + labs(x = "Player Type", y = "Mean Endgame Position") + theme(axis.text = element_text(size = 15), axis.title = element_text(size = 15, face = "bold")) + scale_x_discrete(labels = as.character(c("Constructive\nCollectivist","Constructive\nIndividualist","Disruptive\nCollectivist","Disruptive\nIndividualistic","Random")))  
 suppressMessages(ggsave(sprintf("plots/Positions.png")))
 
-# plot <- ggplot(gameresultslog, aes(x = gameresultslog$type, fill = gameresultslog$outcome)) + geom_bar(color="black", position="fill") 
-# plot <- plot + theme(axis.text=element_text(size=15), axis.title=element_text(size=15,face="bold")) + scale_x_discrete(labels = as.character(c("Constructive\nCollectivist","Constructive\nIndividualist","Disruptive\nCollectivist","Disruptive\nIndividualistic","Random")))  
-# suppressMessages(ggsave(sprintf("plots/OutcomeFrequencies.png")))
+
+
+feltEmotionsLog <- read.csv(file="input/feltEmotionsLog.csv", header=TRUE, sep=",")
+plot <- ggplot(feltEmotionsLog, aes(x = feltEmotionsLog$playerType, fill = feltEmotionsLog$emotionType)) + geom_bar(color="black", position="fill") 
+plot <- plot + labs(x = "Player Type", y = "Times Emotion was Triggered", fill = "Emotion Type") + theme(axis.text=element_text(size = 15), axis.title=element_text(size = 15, face = "bold")) + scale_x_discrete(labels = as.character(c("Constructive\nCollectivist","Constructive\nIndividualist","Disruptive\nCollectivist","Disruptive\nIndividualistic","Random")))  
+suppressMessages(ggsave(sprintf("plots/Emotions.png")))
+
+
+agg <- aggregate(econ_history_perc ~ type, gameresultslog, mean)
+print(agg)
+plot <- ggplot(agg, aes(x = agg$type)) + geom_bar(fill=agg$type, y = agg$econ_history_perc ,color="black", stat="identity", fill = "#638ad3", position = "dodge")  + geom_bar(fill=agg$type, y = agg$econ_history_perc ,color="black", stat="identity", fill = "#638ad3", position = "dodge") 
+plot <- plot + labs(x = "Player Type", y = "Avg. Investments (%)", fill = "Investment Target") + theme(axis.text=element_text(size = 15), axis.title=element_text(size = 15, face = "bold")) + scale_x_discrete(labels = as.character(c("Constructive\nCollectivist","Constructive\nIndividualist","Disruptive\nCollectivist","Disruptive\nIndividualistic","Random")))  
+suppressMessages(ggsave(sprintf("plots/Investments.png")))
